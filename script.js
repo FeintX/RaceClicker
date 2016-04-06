87c56f53bbad16ccd8daf3476585e2d348e7e6ed
FeintX <enkryptx@gmail.com>
2016-04-05 21:42:52 -0400

    Added files via upload

--- script.js
+++ script.js
@@ -11,33 +11,45 @@ $("#result-box").fadeOut(0);
 
 
 var savedVariables = ["driverLevel", "driverUpgradeCost", "carLevel",
-    "raceLevel", "raceUpgradeCost", "racePrize", "engineUpgradeCurrent",
-    "suspUpgradeCurrent", "transUpgradeCurrent", "tireUpgradeCurrent",
+    "raceLevel", "raceUpgradeCost", "racePrize", "engineCurrent",
+    "suspensionCurrent", "transmissionCurrent", "tiresCurrent",
     "cashFlow", "workClicks", "workValue"];
 
-/*  Declaring variables for car upgrade names, costs, and values.  I could
-    probably do these as objects and it would look more elegant. */
-var engineUpgrades = ["Cold Air Intake", "Performance Exhaust System",
-    "ECU Tune", "Stage 2 Camshaft", "Race Pistons and Crankshaft",
-    "Full Race Exhaust", "Racing Camshaft"];
-var engineUpgradeCost = [150, 250, 500, 750, 1500, 2000, 2500];
-var engineUpgradeValue = [1, 1, 2, 2, 2, 2, 2];
+/*  Declaring variables for car upgrade names, costs, and values. */
+var engine = {
+    type: "engine",
+    name: ["Cold Air Intake", "Performance Exhaust System",
+        "ECU Tune", "Stage 2 Camshaft", "Race Pistons and Crankshaft",
+        "Full Race Exhaust", "Racing Camshaft"],
+    cost: [150, 250, 500, 750, 1500, 2000, 2500],
+    value: [1, 1, 2, 2, 2, 2, 2]
+}
 
-var suspUpgrades = ["Performance Struts and Springs",
-    "Coilover Suspension", "Full Race Suspension"];
-var suspUpgradeCost = [500, 2000, 5000];
-var suspUpgradeValue = [2, 2, 3];
+var suspension = {
+    type: "suspension",
+    name: ["Performance Struts and Springs",
+        "Coilover Suspension", "Full Race Suspension"],
+    cost: [500, 2000, 5000],
+    value: [2, 2, 3]
+}
 
-var transUpgrades = ["5-Speed Manual Transmission", "Stage II Clutch",
-    "Limited Slip Differential", "Carbon Fiber Driveshaft",
-    "Racing Rear Axle", "Racing Clutch", "Racing Sequential Transmission"];
-var transUpgradeCost = [750, 1000, 1500, 1750, 2000, 2500, 5000];
+var transmission = {
+    type: "transmission",
+    name: ["5-Speed Manual Transmission", "Stage II Clutch",
+        "Limited Slip Differential", "Carbon Fiber Driveshaft",
+        "Racing Rear Axle", "Racing Clutch", "Racing Sequential Transmission"],
+    cost: [750, 1000, 1500, 1750, 2000, 2500, 5000],
+    value: [2, 2, 2, 2, 2, 2, 3]
+}
 
-var tireUpgrades = ["Performance All Season Tires",
-    "High Performance Summer Tires", "Extreme Performance Summer Tires",
-    "Semi Slick Racing Tires", "Slick Racing Tires"];
-var tireUpgradeCost = [1000, 2000, 3000, 5000, 10000];
-var tireUpgradeValue = [2, 2, 3, 3, 4];
+var tires = {
+    type: "tires",
+    name: ["Performance All Season Tires",
+        "High Performance Summer Tires", "Extreme Performance Summer Tires",
+        "Semi Slick Racing Tires", "Slick Racing Tires"],
+    cost: [1000, 2000, 3000, 5000, 10000],
+    value: [2, 2, 3, 3, 4]
+}
 
 /*  Check if there is saved game data and loads it if it exists.
     Otherwise sets game to starting state. */
@@ -47,7 +59,6 @@ if (localStorage.getItem("cashFlow") != null) {
     resetGame();
 }
 
-/*  Set game to starting state */
 function resetGame() {
     localStorage.clear();
     driverLevel = 50;
@@ -56,10 +67,10 @@ function resetGame() {
     raceLevel = 1;
     raceUpgradeCost = 100;
     racePrize = 50;
-    engineUpgradeCurrent = 0;
-    suspUpgradeCurrent = 0;
-    transUpgradeCurrent = 0;
-    tireUpgradeCurrent = 0;
+    engineCurrent = 0;
+    suspensionCurrent = 0;
+    transmissionCurrent = 0;
+    tiresCurrent = 0;
     cashFlow = 0;
     workClicks = 0;
     workValue = 1;
@@ -68,15 +79,13 @@ function resetGame() {
 }
 
 function saveGame() {
-    var i;
-    for (i in savedVariables) {
+    for (var i in savedVariables) {
         storeVariable(savedVariables[i]);
     }
 }
 
 function loadGame() {
-    var i;
-    for (i in savedVariables) {
+    for (var i in savedVariables) {
         window[savedVariables[i]] = loadVariable(savedVariables[i]);
     }
     disableStart = false;
@@ -92,13 +101,6 @@ function loadVariable(variableName) {
     var temp = localStorage.getItem(variableName);
     return JSON.parse(temp);
 }
-
-$("#start-race").click(function() {
-    if (disableStart == false) {
-        toggleStartButton();
-        runRace();
-    }
-})
 
 function runRace() {
     var oppRace = 21000 - ((raceLevel - 1) * 1750) -
@@ -145,14 +147,106 @@ function runRace() {
     }
 }
 
+function updateStats() {
+    $("#money").text("Cash: $" + cashFlow);
+    $("#driver-level").text("Driver Skill: " + driverLevel);
+    $('#upgrade-driver').text("Train Driver ($" + driverUpgradeCost + ")")
+    $("#car-level").text("Car Performance: " + carLevel);
+    $("#race-level").text("Race Level: " + raceLevel);
+    $('#upgrade-race').text("Next Race Level ($" + raceUpgradeCost + ")")
+    $("#work").text("Work +$" + workValue);
+
+    checkComponent(engine);
+    checkComponent(suspension);
+    checkComponent(transmission);
+    checkComponent(tires);
+
+    if (driverUpgradeCost > cashFlow) {
+        $("#upgrade-driver").addClass("disabled");
+    } else if (driverUpgradeCost <= cashFlow) {
+        $("#upgrade-driver").removeClass("disabled");
+    }
+
+    if (raceUpgradeCost > cashFlow) {
+        $("#upgrade-race").addClass("disabled");
+    } else if (raceUpgradeCost <= cashFlow) {
+        $("#upgrade-race").removeClass("disabled");
+    }
+}
+
+function checkComponent(component) {
+    var current = window[component.type + "Current"];
+    var selector = "#car-" + component.type;
+    if (current == component.name.length) {
+        $(selector).addClass("disabled").
+            text("Max Upgrades");
+    } else {
+        $(selector).text(component.name[current] + " $" +
+            component.cost[current]);
+    }
+    if (component.cost[current] > cashFlow) {
+        $(selector).addClass("disabled");
+    } else if (component.cost[current] <= cashFlow) {
+        $(selector).removeClass("disabled");
+    }
+}
+
+function toggleStartButton() {
+    if (disableStart == true) {
+        disableStart = false;
+        $("#start-race").removeClass("disabled").text("Start Race!");
+
+    } else {
+        disableStart = true;
+        $("#start-race").addClass("disabled").text("Racing");
+    }
+}
+
+function upgradeDriver() {
+    if (driverUpgradeCost <= cashFlow) {
+        cashFlow = cashFlow - driverUpgradeCost;
+        driverLevel ++;
+        driverUpgradeCost = Math.floor(driverUpgradeCost * 1.1);
+        updateStats();
+    }
+}
+
+function upgradeRace() {
+    if (raceUpgradeCost <= cashFlow) {
+        cashFlow = cashFlow - raceUpgradeCost;
+        raceLevel ++;
+        racePrize += raceLevel * 25;
+        raceUpgradeCost = Math.floor(raceUpgradeCost * 2);
+        updateStats();
+    }
+}
+
+function upgradeComponent(component) {
+/*    var current = window[component.type + "Current"];*/
+    if (window[component.type + "Current"] < component.name.length) {
+        if (component.cost[window[component.type + "Current"]] <= cashFlow) {
+            cashFlow -= component.cost[window[component.type + "Current"]];
+            carLevel += component.value[window[component.type + "Current"]];
+            window[component.type + "Current"] ++;
+            updateStats();
+        }
+    }
+}
+
+$("#start-race").click(function() {
+    if (disableStart == false) {
+        toggleStartButton();
+        runRace();
+    }
+})
+
 $("#work").click(function() {
     cashFlow += workValue;
     workClicks ++;
-    if (workClicks >= Math.pow(100, workValue)) {
+    if (workClicks >= (Math.pow(workValue, workValue) * 10)) {
         workValue ++;
     }
     updateStats();
-
 })
 
 $("#upgrade-driver").click(function() {
@@ -171,169 +265,20 @@ $("#save").click(function() {
     saveGame();
 })
 
-// Updating Stats
-function updateStats() {
-    $("#money").text("Cash: $" + cashFlow);
-    $("#driver-level").text("Driver Skill: " + driverLevel);
-    $('#upgrade-driver').text("Train Driver ($" + driverUpgradeCost + ")")
-    $("#car-level").text("Car Performance: " + carLevel);
-    $("#race-level").text("Race Level: " + raceLevel);
-    $('#upgrade-race').text("Next Race Level ($" + raceUpgradeCost + ")")
-    $("#work").text("Work +$" + workValue);
-
-    // Engine Components
-    if (engineUpgradeCurrent == engineUpgrades.length) {
-        $("#car-engine").addClass("disabled").
-            text("Max Upgrades");
-    } else {
-        $("#car-engine").text(engineUpgrades[engineUpgradeCurrent] + " $" +
-            engineUpgradeCost[engineUpgradeCurrent]);
-    }
-    if (engineUpgradeCost[engineUpgradeCurrent] > cashFlow) {
-        $("#car-engine").addClass("disabled");
-    } else if (engineUpgradeCost[engineUpgradeCurrent] <= cashFlow) {
-        $("#car-engine").removeClass("disabled");
-    }
-
-    // Suspension Components
-    if (suspUpgradeCurrent == suspUpgrades.length) {
-        $("#car-suspension").addClass("disabled").
-            text("Max Upgrades");
-    } else {
-        $("#car-suspension").text(suspUpgrades[suspUpgradeCurrent] + " $" +
-            suspUpgradeCost[suspUpgradeCurrent]);
-    }
-    if (suspUpgradeCost[suspUpgradeCurrent] > cashFlow) {
-        $("#car-suspension").addClass("disabled");
-    } else if (suspUpgradeCost[suspUpgradeCurrent] <= cashFlow) {
-        $("#car-suspension").removeClass("disabled");
-    }
-
-    // Transmission Components
-    if (transUpgradeCurrent == transUpgrades.length) {
-        $("#car-transmission").addClass("disabled").
-            text("Max Upgrades");
-    } else {
-        $("#car-transmission").text(transUpgrades[transUpgradeCurrent] + " $" +
-            transUpgradeCost[transUpgradeCurrent]);
-    }
-    if (transUpgradeCost[transUpgradeCurrent] > cashFlow) {
-        $("#car-transmission").addClass("disabled");
-    } else if (transUpgradeCost[transUpgradeCurrent] <= cashFlow) {
-        $("#car-transmission").removeClass("disabled");
-    }
-
-    // Tire Components
-    if (tireUpgradeCurrent == tireUpgrades.length) {
-        $("#car-tires").addClass("disabled").
-            text("Max Upgrades");
-    } else {
-        $("#car-tires").text(tireUpgrades[tireUpgradeCurrent] + " $" +
-           tireUpgradeCost[tireUpgradeCurrent]);
-    }
-    if (tireUpgradeCost[tireUpgradeCurrent] > cashFlow) {
-        $("#car-tires").addClass("disabled");
-    } else if (tireUpgradeCost[tireUpgradeCurrent] <= cashFlow) {
-        $("#car-tires").removeClass("disabled");
-    }
-
-    if (driverUpgradeCost > cashFlow) {
-        $("#upgrade-driver").addClass("disabled");
-    } else if (driverUpgradeCost <= cashFlow) {
-        $("#upgrade-driver").removeClass("disabled");
-    }
-
-    if (raceUpgradeCost > cashFlow) {
-        $("#upgrade-race").addClass("disabled");
-    } else if (raceUpgradeCost <= cashFlow) {
-        $("#upgrade-race").removeClass("disabled");
-    }
-}
-
-//Toggle Start button
-function toggleStartButton() {
-    if (disableStart == true) {
-        disableStart = false;
-        $("#start-race").removeClass("disabled").text("Start Race!");
-
-    } else {
-        disableStart = true;
-        $("#start-race").addClass("disabled").text("Racing");
-    }
-}
-
-// Upgrading Driver skill
-function upgradeDriver() {
-    if (driverUpgradeCost <= cashFlow) {
-        cashFlow = cashFlow - driverUpgradeCost;
-        driverLevel ++;
-        driverUpgradeCost = Math.floor(driverUpgradeCost * 1.1);
-        updateStats();
-    }
-}
-
-// Upgrading race level
-function upgradeRace() {
-    if (raceUpgradeCost <= cashFlow) {
-        cashFlow = cashFlow - raceUpgradeCost;
-        raceLevel ++;
-        racePrize += raceLevel * 25;
-        raceUpgradeCost = Math.floor(raceUpgradeCost * 2);
-        updateStats();
-    }
-}
-
-
-// Upgrade Engine Components
 $("#car-engine").click(function() {
-    var max = engineUpgrades.length;
-    if (engineUpgradeCurrent < max) {
-        if (engineUpgradeCost[engineUpgradeCurrent] <= cashFlow) {
-            cashFlow -= engineUpgradeCost[engineUpgradeCurrent];
-            carLevel += engineUpgradeValue[engineUpgradeCurrent];
-            engineUpgradeCurrent ++;
-            updateStats();
-        }
-    }
+    upgradeComponent(engine);
 })
 
-// Upgrade Suspension Components
 $("#car-suspension").click(function() {
-    var max = suspUpgrades.length;
-    if (suspUpgradeCurrent < max) {
-        if (suspUpgradeCost[suspUpgradeCurrent] <= cashFlow) {
-            cashFlow -= suspUpgradeCost[suspUpgradeCurrent];
-            carLevel += suspUpgradeValue[suspUpgradeCurrent];
-            suspUpgradeCurrent ++;
-            updateStats();
-        }
-    }
+    upgradeComponent(suspension);
 })
 
-// Upgrade Transmission Components
 $("#car-transmission").click(function() {
-    var max = transUpgrades.length;
-    if (transUpgradeCurrent < max) {
-        if (transUpgradeCost[transUpgradeCurrent] <= cashFlow) {
-            cashFlow -= transUpgradeCost[transUpgradeCurrent];
-            carLevel += transUpgradeValue[transUpgradeCurrent];
-            transUpgradeCurrent ++;
-            updateStats();
-        }
-    }
+    upgradeComponent(transmission);
 })
 
-// Upgrade Tire Components
 $("#car-tires").click(function() {
-    var max = tireUpgrades.length;
-    if (tireUpgradeCurrent < max) {
-        if (tireUpgradeCost[tireUpgradeCurrent] <= cashFlow) {
-            cashFlow -= tireUpgradeCost[tireUpgradeCurrent];
-            carLevel += tireUpgradeValue[tireUpgradeCurrent];
-            tireUpgradeCurrent ++;
-            updateStats();
-        }
-    }
+    upgradeComponent(tires);
 })
 
 // Toggles visual button state on mouseover
